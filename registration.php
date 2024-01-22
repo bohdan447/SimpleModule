0f27ec716784a84d168d07131ff4202396e29ef4
Bohdan <bogdan021105@gmail.com>
2024-01-22 19:16:46 +0200

    Rollback

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Perspective_HelloWorld1',
+    'Perspective_HelloWorld',
 __DIR__
 );
